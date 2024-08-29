03c2d8940ed3933c4af1f999cddf109a3e84e57c
Jexnsinho <jeanpaulrosa4@gmail.com>
2024-08-29 09:14:53 -0500

    Adición de un menu principal

---
+++ Ejemplo.py
@@ -0,0 +1,9 @@
+def menu():
+    print("\t1- Opción 1")
+    print("\t2- Opción 2")
+    print("\t3- Opción 3")
+    print("\t4- Opción 4")
+    print("\t5- Opción 5")
+     
+menu()
+opc = int(input("Escoja una opción: "))
